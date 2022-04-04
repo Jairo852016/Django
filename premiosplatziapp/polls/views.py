c9b1af5847ca2356d5c6d47aa116d93a75f53c42
Jairo Perez <jairo85cd@gmail.com>
2022-04-04 09:18:51 -0500

    view

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render,get_list_or_404
 from django.http import HttpRequest, HttpResponse
-
+from .models import Question
 # Create your views here.
 def index(request):
-    return HttpResponse("Hola Mundo")
+    lasted_question_list=Question.objects.all()
+    return render(request,"polls/index.html",{
+        "lasted_question_list":lasted_question_list
+    })
+    #HttpResponse("Estas en la pagina principal")
+
+def detail(request,question_id):
+    question=get_object_or_404(Question, pk=question_id)
+    return   render(request,"polls/detail.html",{
+        "question":question
+    })
+    #HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+
+
+def results(request,question_id):
+    return HttpResponse(f"Estas viendo los resultados la pregunta numero {question_id}")
+
+
+
+def vote(request,question_id):
+    return HttpResponse(f"Estas votando a la pregunta numero {question_id}")
